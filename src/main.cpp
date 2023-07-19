a659c84fdb93eba17f7ab073b5eb6a3dec8c49a7
axelKeizoStahl <axelkeizo@gmail.com>
2023-07-19 00:05:28 -0400

    now working

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@
 BearSSL::WiFiClientSecure client;
 ChatGPT<BearSSL::WiFiClientSecure> chat_gpt(&client, "v1", openai_api_key);
 
-void processJsonResponse(const String& jsonResponse);
+inline bool message(String& result,
+                    const String& model,
+                    const String& role);
 
 void setup() {
   Serial.begin(115200);
@@ -17,21 +19,39 @@ void setup() {
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Connecting...");
-    delay(500);
+    delay(1500);
   }
   Serial.println("Connected!");
-
-  client.setInsecure();
-
-  String result;
-  bool call = chat_gpt.simple_message("gpt-3.5-turbo", "user", "Hello", result);
-  //if (chat_gpt.simple_message("gpt-3.5-turbo-0301", "user", "Planning a 3-day trip to San Diego", result)) {
-  //Serial.println("===OK===");
-  Serial.println(result);
-  /*} else {
-    Serial.println("===ERROR===");
-    Serial.println(result);
-  }*/
 }
 
-void loop() {}
+void loop() {
+  client.setInsecure();
+  String result;
+  if (!message(result, "gpt-3.5-turbo", "user")) {
+    Serial.println("===ERROR===");
+    Serial.println("Please try again.");
+  }
+}
+
+inline bool message(String& result,
+                    const String& model,
+                    const String& role) {
+  while (Serial.available()) {
+    Serial.read();
+  }
+  String input;
+  Serial.println("Enter your message: ");
+  while (!input.endsWith("\n")) {
+    input += Serial.readString();
+  }
+  input.trim();
+  Serial.println("Synthesizing response...");
+  if (chat_gpt.simple_message("gpt-3.5-turbo", "user", input, result)) {
+    Serial.println(result);
+    return true;
+  } else {
+    Serial.println("===ERROR===");
+    Serial.println(result);
+    return false;
+  }          
+} 
