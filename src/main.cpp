db3980dd174f7729c915c2aaa9ff73d667fc7ad0
axelKeizoStahl <axelkeizo@gmail.com>
2023-07-20 21:51:34 -0400

    added snake_casing

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void scan_and_display_networks();
 
 inline bool connect(String ssid, String password, int timeout);
 
-inline bool user_message(String& result,
+inline bool user_message(String& result, //this function actually takes input and displays output
                     const String& model,
                     const String& role);
 
@@ -21,7 +21,7 @@ inline bool chatgpt_req(BearSSL::WiFiClientSecure& client,
                         const String& content,
                         String& result);
 
-void processJsonResponse(const String& jsonResponse);
+void process_json_response(const String& json_response);//needed to parse json for response
 
 void setup() {
   Serial.begin(115200);
@@ -53,7 +53,7 @@ void setup() {
   Serial.println("Connected.");
 }
 void loop() {
-  client.setInsecure();
+  client.setInsecure();//so we dont need a certificate for https reqs
   String result;
   if (!user_message(result, "gpt-3.5-turbo", "user")) {
     Serial.println("===ERROR===");
@@ -62,20 +62,20 @@ void loop() {
 }
 
 void scan_and_display_networks() {
-  int numberOfNetworks = WiFi.scanNetworks();
-  for (int i = 0; i < numberOfNetworks; i++) {
+  int number_of_networks = WiFi.scanNetworks();
+  for (int i = 0; i < number_of_networks; i++) {//we may want to sort by signal strength and only display the top amount at a later point in time
     Serial.print("Network name: ");
     Serial.println(WiFi.SSID(i));
     Serial.print("Signal strength: ");
     Serial.println(WiFi.RSSI(i));
     Serial.println("-----------------------");
-    delay(1000);
+    delay(600);//only reason for delay is to make it easier to read
   }
 }
 
 inline bool connect(String ssid, String password, int timeout) {
   WiFi.begin(ssid, password);
-  int attempts = 0;
+  int attempts = 0;//connection attempts
   while (WiFi.status() != WL_CONNECTED and  attempts < timeout) {
     Serial.println("Connecting...");
     delay(1500);
@@ -98,7 +98,7 @@ inline bool user_message(String& result,
   input.trim();
   Serial.println("Synthesizing response...");
   if (chatgpt_req(client, String(openai_api_key), "gpt-3.5-turbo", "user", input, result)) {
-    processJsonResponse(result);
+    process_json_response(result);
     return true;
   } else {
     Serial.println("===ERROR===");
@@ -113,7 +113,7 @@ inline bool chatgpt_req(BearSSL::WiFiClientSecure& client,
                         const String& role,
                         const String& content,
                         String& result) {
-  String responseStr;
+  String response_str;
   String api_version = "v1";
   String host = "api.openai.com";
   String auth_header = "Authorization: Bearer " + api_key;
@@ -123,26 +123,26 @@ inline bool chatgpt_req(BearSSL::WiFiClientSecure& client,
     client.print(payload);
     while (client.connected()) {
       if (client.available()) {
-        responseStr += client.readString();
+        response_str += client.readString();
       }
     }
     client.stop(); 
-    int responseCode = 0;
-    if (responseStr.indexOf(" ") != -1) {
-      responseCode = responseStr.substring(responseStr.indexOf(" ") + 1, responseStr.indexOf(" ") + 4).toInt();
+    int response_code = 0;
+    if (response_str.indexOf(" ") != -1) {
+      response_code = response_str.substring(response_str.indexOf(" ") + 1, response_str.indexOf(" ") + 4).toInt();
     }
 
-    if (responseCode != 200) {
-      result = responseStr;
+    if (response_code != 200) {
+      result = response_str;
       return false;
     } else {
 
-      int start = responseStr.indexOf("{");
-      int end = responseStr.lastIndexOf("}");
-      String jsonBody = responseStr.substring(start, end + 1);
+      int start = response_str.indexOf("{");
+      int end = response_str.lastIndexOf("}");
+      String json_body = response_str.substring(start, end + 1);
 
-      if (jsonBody.length() > 0) {
-        result = jsonBody;
+      if (json_body.length() > 0) {
+        result = json_body;
         return true;
       }
 
@@ -156,12 +156,12 @@ inline bool chatgpt_req(BearSSL::WiFiClientSecure& client,
   }
 }
 
-void processJsonResponse(const String& jsonResponse) {
-  DynamicJsonDocument doc(jsonResponse.length() + 200);
+void process_json_response(const String& json_response) {
+  DynamicJsonDocument doc(json_response.length() + 200);
 
-  DeserializationError error = deserializeJson(doc, jsonResponse.c_str());
+  DeserializationError error = deserializeJson(doc, json_response.c_str());
 
-  if (error) {
+  if (error) {//we may or may not want all of this error handling
     Serial.print("deserializeJson() failed: ");
     Serial.println(error.c_str());
     return;
@@ -172,28 +172,28 @@ void processJsonResponse(const String& jsonResponse) {
     return;
   }
 
-  JsonArray choicesArray = doc["choices"].as<JsonArray>();
+  JsonArray choices_array = doc["choices"].as<JsonArray>();
 
-  if (choicesArray.size() == 0) {
+  if (choices_array.size() == 0) {
     Serial.println("JSON response 'choices' array is empty.");
     return;
   }
 
-  JsonObject firstChoice = choicesArray[0].as<JsonObject>();
+  JsonObject first_choice = choices_array[0].as<JsonObject>();
 
-  if (!firstChoice.containsKey("message")) {
+  if (!first_choice.containsKey("message")) {
     Serial.println("JSON response element does not contain 'message' object.");
     return;
   }
 
-  JsonObject messageObject = firstChoice["message"].as<JsonObject>();
+  JsonObject message_object = first_choice["message"].as<JsonObject>();
 
-  if (!messageObject.containsKey("content")) {
+  if (!message_object.containsKey("content")) {
     Serial.println("JSON response 'message' object does not contain 'content' field.");
     return;
   }
 
-  const char* messageContent = messageObject["content"].as<const char*>();
+  const char* message_content = message_object["content"].as<const char*>();
 
-  Serial.println(messageContent);
+  Serial.println(message_content);
 }
